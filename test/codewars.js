29846572c9b8a4e57f648c7d84d3f5d6c2e42bcb
Ivan Tolkachev <vantrey@yandex.ru>
2020-02-07 10:50:55 +0300

    2 codewars solutions

--- test/codewars.js
+++ test/codewars.js
@@ -1,3 +1,4 @@
+//string revesce
 function solution(str){
   var strMass=[];
   for (var i=0; i<str.length; i++) {
@@ -7,3 +8,16 @@ function solution(str){
 } // my solution
 
 const solution = str => str.split('').reverse().join(''); // other solution
+
+//smaller element
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    let min=args[0];
+    for (let i=0; i<args.length; i++) {
+      if (args[i]<min) {
+        min=args[i];
+        }
+    }
+    return min;
+  }
+}
